b01b313ff6c3bdcd2f3215ce62470a900c478c58
vitorribeiro3988-sys <vitorribeiro3988@gmail.com>
2026-05-08 12:14:23 -0300

    SexoFM.cs

---
+++ SexoFM.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valor_Positivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+            do
+            {
+                Console.Write("Sexo (F/M): ");
+                sexo = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            } while (sexo != 'F' && sexo != 'f' && sexo != 'M' && sexo != 'm');
+        }
+    }
+}
